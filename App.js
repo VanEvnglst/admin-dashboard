8aef350c0d2ea8fd8d091f2a0ec796a6894967dc
VanEvnglsta <54834442+VanEvnglsta@users.noreply.github.com>
2021-03-10 07:48:57 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Dashboard" component={Dashboard}/>
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+    
+  );
+};
+
+export default App;
