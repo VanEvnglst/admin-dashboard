a8cf02b17baa1f027176f4a126fac067de83fc71
James Patrick <jamespatrick.debelen@tup.edu.ph>
2021-03-25 21:31:19 +0800

    added table in products section

--- app/screens/products/index.js
+++ app/screens/products/index.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import styles from './styles';
+import {StyleSheet, ScrollView, View} from 'react-native';
+import {Table, Row, Rows} from 'react-native-table-component';
 
-const Products = () => {
+export default class App extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      HeadTable: ['SKU', 'ITEM NAME', 'CATEGORY', 'PRICE'],
+      DataTable: [
+        ['A1', 'Chicken Adobo', 'Combo Meal', '120'],
+        ['A2', 'Beef Caldereta', 'Combo Meal', '120'],
+        ['A3', 'Mechado', 'Combo Meal', '110'],
+        ['A4', 'Lechon Kawali', 'Combo Meal', '150'],
+        ['A5', 'Sisig', 'Combo Meal', '130'],
+        ['B1', 'Cheese Burger', 'Burger', '70'],
+        ['B2', 'Double Cheeseburger', 'Burger', '80'],
+        ['B3', 'Quarter Pounder', 'Burger', '80'],
+        ['B4', 'Double Quarter Pounder', 'Burger', '90'],
+        ['B5', 'Hamburger', 'Burger', '60'],
+        ['C1', 'Leche Flan', 'Dessert', '100'],
+        ['C2', 'Ube Macapuno', 'Dessert', '100'],
+        ['C3', 'Chocolate Cake', 'Dessert', '120'],
+        ['C4', 'Cookies and Cream Sundae', 'Dessert', '80'],
+        ['C5', 'Apple Pie', 'Dessert', '40'],
+        ['D1', 'Coke 1L', 'Beverages', '50'],
+        ['D2', 'Sprite 1L', 'Beverages', '50'],
+        ['D3', 'Royal 1L', 'Beverages', '50'],
+        ['D4', 'Nestea 1L', 'Beverages', '50'],
+        ['D5', 'Pineapple Juice 1L', 'Beverages', '50'],
+        ['', '', '', ''],
+      ],
+    };
+  }
 
-  return(
-  <View>
-    <Text>Products</Text>
-  </View>
-  );
+  render () {
+    const state = this.state;
+    return (
+      <View style={styles.container}>
+        <Table borderStyle={{borderWidth: 1, borderColor: 'transparent'}}>
+          <Row
+            data={state.HeadTable}
+            style={styles.HeadStyle}
+            textStyle={styles.TableText}
+          />
+          <ScrollView>
+            <Rows data={state.DataTable} textStyle={styles.TableText} />
+          </ScrollView>
+        </Table>
+      </View>
+    );
+  }
 }
-
-export default Products;
--- app/screens/products/styles.js
+++ app/screens/products/styles.js
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({
-  container: {
-  
-  }
+export default StyleSheet.create ({
+  container: {flex: 1, padding: 20, paddingTop: 20, backgroundColor: '#fff'},
+  HeadStyle: {height: 50, backgroundColor: '#D4D4D4'},
+  TableText: {margin: 10, textAlign: 'center'},
 });
