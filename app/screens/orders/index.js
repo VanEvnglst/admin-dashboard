428c73d86c2ac03aa25c66d8d9f99c8551a12a3b
kalayaan13 <kalcentsantiago@gmail.com>
2021-03-28 20:38:50 +0800

    Create Order page.

--- app/screens/orders/index.js
+++ app/screens/orders/index.js
@@ -1,13 +1,139 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+// import React from 'react';
+import * as React from 'react';
+import { Text, View, ScrollView, StyleSheet }  from 'react-native';
+import styles from './styles';
 
-const Orders = () => {
+
+export default Orders = () => {
 
   return(
-  <View>
-    <Text>Orders</Text>
-  </View>
-  );
-}
+    <ScrollView>
+            <View style={{flex:1}}>
 
-export default Orders;
+            <View>
+            <Text style={styles.orderHistoryTitle}>Order History</Text>
+            <Text style={styles.orderDate}>Monday, 01 March 2021</Text>
+            </View>
+
+              <View style={styles.container}>
+        
+                  <View style={styles.orderNumberContainer}>
+                    <Text style={styles.orderNumberTitle}>ORDER</Text>
+                    <Text style={styles.orderNumber}>30</Text>
+                  </View>
+
+
+                  <View style={styles.orderContentContainer}>
+                    <Text style={styles.orderPrice}>PHP 150.00</Text>
+                    <Text style={styles.orderNumberReference}>Order no: #000001</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  </View>
+
+
+                  <View style={styles.orderStatusContainer}>
+                    <Text style={styles.orderStatus}>Completed</Text>
+                  </View>
+              </View>
+
+
+
+              <View style={styles.container}>
+
+                  <View style={styles.orderNumberContainer}>
+                    <Text style={styles.orderNumberTitle}>ORDER</Text>
+                    <Text style={styles.orderNumber}>30</Text>
+                  </View>
+
+                  <View style={styles.orderContentContainer}>
+                    <Text style={styles.orderPrice}>PHP 150.00</Text>
+                    <Text style={styles.orderNumberReference}>Order no: #000001</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                    <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  </View>
+
+                  <View style={styles.orderStatusContainer}>
+                    <Text style={styles.orderStatus}>Completed</Text>
+                  </View>
+              </View> 
+
+
+
+              <View style={styles.container}>
+
+                <View style={styles.orderNumberContainer}>
+                  <Text style={styles.orderNumberTitle}>ORDER</Text>
+                  <Text style={styles.orderNumber}>30</Text>
+                </View>
+
+                <View style={styles.orderContentContainer}>
+                  <Text style={styles.orderPrice}>PHP 150.00</Text>
+                  <Text style={styles.orderNumberReference}>Order no: #000001</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                </View>
+
+                <View style={styles.orderStatusContainer}>
+                  <Text style={styles.orderStatus}>Completed</Text>
+                </View>
+              </View> 
+
+
+
+              <View style={styles.container}>
+
+                <View style={styles.orderNumberContainer}>
+                  <Text style={styles.orderNumberTitle}>ORDER</Text>
+                  <Text style={styles.orderNumber}>30</Text>
+                </View>
+
+                <View style={styles.orderContentContainer}>
+                  <Text style={styles.orderPrice}>PHP 150.00</Text>
+                  <Text style={styles.orderNumberReference}>Order no: #000001</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                </View>
+
+                <View style={styles.orderStatusContainer}>
+                  <Text style={styles.orderStatus}>Completed</Text>
+                </View>
+              </View> 
+
+
+              <View style={styles.container}>
+
+                <View style={styles.orderNumberContainer}>
+                  <Text style={styles.orderNumberTitle}>ORDER</Text>
+                  <Text style={styles.orderNumber}>30</Text>
+                </View>
+
+                <View style={styles.orderContentContainer}>
+                  <Text style={styles.orderPrice}>PHP 150.00</Text>
+                  <Text style={styles.orderNumberReference}>Order no: #000001</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                  <Text style={styles.orderContent}>Wintermelon Milktea</Text>
+                </View>
+
+                <View style={styles.orderStatusContainer}>
+                  <Text style={styles.orderStatus}>Completed</Text>
+                </View>
+              </View> 
+
+
+
+
+          </View>        
+    </ScrollView> 
+  );
+};
+
--- app/screens/orders/styles.js
+++ app/screens/orders/styles.js
@@ -1,7 +1,91 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({
-  container: {
+const styles = StyleSheet.create({
+
+    orderHistoryTitle:{
+      fontWeight: 'bold',
+      fontSize: 24,
+      marginLeft: 18,
+      marginTop: 20,
+    },
+    orderDate:{
+      fontSize: 18,
+      marginBottom: 10,
+      marginLeft: 18,
+    },
   
-  }
-});
+  
+    container:{
+      flexDirection:"row",
+      marginBottom: 10,
+      flex:1,
+    },
+  
+  
+  
+    orderNumberContainer:{
+      padding: 15,
+      backgroundColor: "#E9EAEA",
+      borderTopStartRadius: 20,
+      borderBottomLeftRadius: 20,
+      marginLeft: 8,
+    },
+    orderNumberTitle:{
+      fontSize: 18,
+      marginTop: 25,
+  
+      
+    },
+    orderNumber:{
+      fontSize: 48,
+      marginLeft: 2,
+    },
+  
+  
+  
+    orderContentContainer:{
+      backgroundColor: "#FFFFFF",
+      paddingBottom: 10,
+      paddingLeft: 15,
+      paddingRight: 10,
+  
+  
+    },
+  
+    orderPrice:{
+      fontSize: 14,
+      textAlign: "right",
+      marginBottom: 3,
+    },
+    orderNumberReference:{
+      fontSize: 16,
+      marginBottom: 10,
+      fontWeight: 'bold',
+    },
+    orderContent:{
+      fontSize: 14,
+      marginBottom: 5,
+      marginRight: 50,
+    },
+  
+  
+  
+  
+    orderStatusContainer:{
+      padding:20,
+      backgroundColor: "#A6AFAA",
+      borderTopEndRadius: 20,
+      borderBottomRightRadius: 20,
+    },
+    orderStatus:{
+      fontSize: 13,
+      marginTop: 105,
+      fontWeight: 'bold',
+  
+    },
+  
+  
+  });
+
+  export default styles;
+  
