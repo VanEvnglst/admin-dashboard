d6001bdb4d3a4eff481926584687bd53d1005c3a
James Patrick <jamespatrick.debelen@tup.edu.ph>
2021-04-28 10:19:07 +0800

    TableWrapper and Cell Trial

--- app/screens/products/index.js
+++ app/screens/products/index.js
@@ -1,11 +1,57 @@
 import React, {Component} from 'react';
 import styles from './styles';
 import {StyleSheet, ScrollView, View} from 'react-native';
-import {Table, Row, Rows} from 'react-native-table-component';
+import {Table, Row, TableWrapper, Cell} from 'react-native-table-component';
 import {headTable, dataTable} from './content'
+import database from './../../utils/database';
+import SQLite from 'react-native-sqlite-storage';
 
 export default class ProductsScreen extends Component {
+   constructor(props) {
+   super(props)
+   this.state = {
+    products: [],
+   };
+ }
+  
+  componentDidMount() {
+   console.log('did mount')
+   this.getProducts();
+  }
+
+   getProducts = () => {
+     SQLite.openDatabase(
+      'adminDash.db',
+      '1.0',
+      'Admin Dashboard',
+      '200000'
+      ).then(DB => {
+        db = DB;
+        db.transaction((tx) => {
+          tx.executeSql('SELECT Item, ItemCategory FROM Items', []).then(([tx,results]) => {
+              var len = results.rows.length;
+              var temp = [];
+              for (let i = 0; i < len; i++) {
+                let row = results.rows.item(i)
+                temp.push({'Item': row.Item,'ItemCategory': row.ItemCategory});
+              }
+              console.log(temp);
+              this.setState({ products: temp });
+          }).catch(err => {
+            console.log('exe',err);
+          });
+          }).then((result) => {
+          db.close();
+          }).catch((err) => {
+          console.log(err);
+          });
+      }).catch((err) => {
+      console.log(err);
+      });
+  }
+
   render () {
+    const show = this.state.products;
     return (
       <View style={styles.container}>
         <Table style={styles.tableStyle}>
@@ -15,10 +61,9 @@ export default class ProductsScreen extends Component {
             textStyle={styles.tableText}
           />
           <ScrollView>
-            <Rows 
-              data={dataTable} 
-              textStyle={styles.tableText}
-            />
+              <TableWrapper>
+                  <Cell data= {show.Item}/>
+              </TableWrapper>
           </ScrollView>
         </Table>
       </View>
