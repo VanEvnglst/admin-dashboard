aad609c611688610f56c90f373d9a6f8bb611660
kalayaan13 <kalcentsantiago@gmail.com>
2021-05-12 15:19:45 +0800

    Changes on DATE & OrderDetailsContainer

--- app/screens/orders/index.js
+++ app/screens/orders/index.js
@@ -2,19 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, FlatList, ScrollView, SafeAreaView } from 'react-native';
 import styles from './styles';
 import SQLite from 'react-native-sqlite-storage';
+import moment from "moment";
+
 
 SQLite.DEBUG(true);
 SQLite.enablePromise(true);
 let db;
 
 const OrdersScreen = () => {
-  
+
   const [enableScroll, setScroll] = useState(false);
   const [orderData, setOrderData] = useState([]);
 
-  // mag rrun tayo ng useEffect tapos sa loob tatawagin natin yung function to get the data
-  useEffect(() => {    
+
+  useEffect(() => {
     getOrderContentData();
+
   }, []);
 
   getOrderContentData = () => {
@@ -26,51 +29,55 @@ const OrdersScreen = () => {
     ).then(DB => {
       db = DB;
       db.transaction(tx => {
-        tx.executeSql('SELECT * from SalesTransactions ORDER BY Date DESC').then(([tx, results]) => {
-          var len = results.rows.length;
-          var temp = [];
-          for(i = 0; i < len; i++) {
-            var item = results.rows.item(i);
-            temp.push(item);
-          }
-          setOrderData(temp);
-        }).catch(err => {
-          console.log('execute err', err);
-        })
+        tx.executeSql('SELECT * from SalesTransactions ORDER BY Date DESC')
+          .then(([tx, results]) => {
+            var len = results.rows.length;
+            var temp = [];
+            for (i = 0; i < len; i++) {
+              var item = results.rows.item(i);
+              temp.push(item);
+            }
+            setOrderData(temp);
+          }).catch(err => {
+            console.log('execute err', err);
+          })
       });
     }).catch(err => {
       console.log('open database error', err);
     })
   }
-  // yung empty array sa taas ^ ginagamit para once lang mag rrun yung useEffect natin.
 
-  function renderNewData(item, index) {
+  function renderNewData(item) {
     console.log('item', item);
+    const DATE = moment(item.Date, 'MM/DD/YYYY').format("MMMM DD, YYYY")
     return (
       <View style={styles.orderPageContainer}>
+        <Text style={styles.orderDate}>{DATE}</Text>
         <View style={styles.container}>
           <View style={styles.orderNumberContainer}>
-            <Text style={styles.orderNumberTitle}>{item.orderNumberTitleData}</Text>
-            <Text style={styles.orderNumber}>{item.orderNumberData}</Text>
+            <Text style={styles.orderNumberTitle}>ORDER</Text>
+            <Text style={styles.orderNumber}></Text>
           </View>
 
-          <View style={styles.orderContentContainer}>
-            <Text style={styles.orderPrice}>{item.Amount}</Text>
-            <Text style={styles.orderNumberReference}>{item.TransactionNum}</Text>
-            <View>
-              <Text>Payment Mode:</Text>
-            <Text style={styles.orderContent}>{item.PaymentMode}</Text>
+          <View style={styles.orderDetailsContainer}>
+            <Text style={styles.amount}>{item.Amount}PHP</Text>
+            <Text style={styles.transactionNum}>Order No: #{item.TransactionNum}</Text>
+            <View style={styles.paymentModeContainer}>
+              <Text style={styles.paymentMode}>Payment Mode: {item.PaymentMode}</Text>
             </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between',       alignItems: 'center'}}>
-              <Text>Cashier Name</Text>
-            <Text style={styles.orderContent}>{item.CashierName}</Text>
+            <View style={styles.cashierNameContainer}>
+              <Text style={styles.cashierName}>Cashier Name: {item.CashierName}</Text>
             </View>
-            <Text style={styles.orderContent}>{item.orderContent1Data}</Text>
-            <Text style={styles.orderContent}>{item.orderContent1Data}</Text>
+            <View style={styles.totalItemsWithVoid}>
+              <Text style={styles.cashierName}>TotalItemsWithVoid: {item.TotalItemsWithVoid}</Text>
+            </View>
+            <View style={styles.totalAmountWithVoid}>
+              <Text style={styles.cashierName}>TotalAmountWithVoid: {item.TotalAmountWithVoid}</Text>
+            </View>
           </View>
 
-          <View style={styles.orderStatusContainer}>
-            <Text style={styles.orderStatus}>{item.orderStatusData}</Text>
+          <View style={styles.statusContainer}>
+            <Text style={styles.status}>{item.Status}</Text>
           </View>
         </View>
       </View>
@@ -78,38 +85,31 @@ const OrdersScreen = () => {
   };
 
   return (
-    // SafeAreaView usually nilalagay as pinaka parent,
-    // ang use nito ay para yung content natin hindi matago sa phones na may notch
     <SafeAreaView>
       <View
         onStartShouldSetResponderCapture={() => setScroll(true)}
       >
         <ScrollView scrollEnabled={enableScroll}>
           <View style={styles.orderPageContainer}>
-          {/* TITLE */}
+            {/* TITLE */}
             <View style={styles.titleWrapper}>
               <Text style={styles.orderHistoryTitle}>Order History</Text>
-              {/* Move your date text na maging part ng renderItem method */}
-              {/* Stick mo nalang yung format to be MM/DD/YY */}
-              <Text style={styles.orderDate}>March 30, 2021</Text>
             </View>
 
-          {/* Order Items */}
-          <View>
+            {/* Order Items */}
+            <View>
             <FlatList
-              data={orderData}
-              keyExtractor={item => item.id}
-              vertical={true}
-              renderItem={({ item, index }) => renderNewData(item, index)}
-            />
+                data={orderData}
+                keyExtractor={item => item.TransactionNum}
+                vertical={true}
+                renderItem={({ item }) => renderNewData(item)}
+              />
+            </View>
           </View>
-        </View>
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default OrdersScreen;
-
-
